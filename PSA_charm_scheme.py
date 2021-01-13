d5e0bb00fc0748ec24b6266fc700200adf89266c
Thomas <thomaslardy@hotmail.fr>
2021-01-13 19:27:26 +0100

    add DD privacy and Pollard algorithm

--- PSA_charm_scheme.py
+++ PSA_charm_scheme.py
@@ -1,10 +1,17 @@
+#Implementation for the Private Stream Aggregation algorithm proposed by Elaine Shi
+#Based on www.elaineshi.com/docs/ndss2011.pdf
 from charm.toolbox.integergroup import IntegerGroup
+from charm.toolbox.integergroup import integer
 from charm.toolbox.hash_module import Hash
 from charm.toolbox.conversion import Conversion
 from math import exp, log
 from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
 import hashlib, secrets
 import scipy.stats as st
+import sys
+from Pollard_algorithm import *
 
 
 def H(t):
@@ -24,8 +31,8 @@ class Geometric(st.rv_discrete):
         One sided : f(k) = (l-1).l**k
     """
     def _pmf(self, k, l):
-        return (l-1)*pow(l, -k)
-        # return (((l-1)/(l+1))*pow(l, -abs(k))) #(l**(-abs(k)))
+        # return (l-1)*pow(l, -k)
+        return (((l-1)/(l+1))*pow(l, -abs(k)))
 
 class Aggregator():
     """
@@ -46,7 +53,8 @@ class Aggregator():
         v1 = pow(ht,self.sk)
         v2 = prod_enc(self.c)
         v = (v1*v2)
-        sum_dec = compute_log(g, v, delta)
+        #sum_dec = compute_log(g, v, delta)
+        sum_dec = integer(pollard_rho(g, v, p, modulus))
         return sum_dec
 
 def prod_enc(c):
@@ -87,6 +95,11 @@ class User():
     def encrypt(self):
         """
             Compute user's ciphertext for time step t (NoisyEnc(param,ski,t,x))
+            input :
+            g : public param - prime order, generator
+            sk : user's secret key
+            ht : hash value for time t
+            x : value to encrypt
             return :
             c : user's ciphertext for time t
         """
@@ -99,13 +112,23 @@ class User():
         """
             Implement the DD-Private Data Randomization Procedure
             Generating and adding the noise necessary for achieving distributed differential privacy
+            return :
+            x_noise : user's data with noise
         """
         bernoulli_dist = st.bernoulli(self.beta) #Bernouilli's law with parameter beta
         if bernoulli_dist.rvs():
-            dist = Geometric(a=1, b=1000, name="Geometric")
+            dist = Geometric(a=-10000, b= 10000, name="Geometric")
             r = dist.rvs(l=self.alpha)
         else:
             r = 0
+        self.x = (self.x + r)%p
+    
+    def naive_scheme(self):
+        """
+            Each user add independant geometric noise to their data
+        """
+        dist = Geometric(a=-10000, b=10000, name="Geometric")
+        r = dist.rvs(l=self.alpha)
         self.x = (self.x + r)%p
 
 def keygen(n, p):
@@ -124,7 +147,7 @@ def keygen(n, p):
             #     number = 0
             number = secrets.choice(l)
         elif sum > p:
-            l = [i for i in range(-p, p - sum)]
+            l = [i for i in range(-p, p - sum+1)]
             # if l == []:
             #     number = 0
             number = secrets.choice(l)
@@ -140,43 +163,76 @@ def keygen(n, p):
 
     return L
 
+def error(nb_users, sk, data_group):
+    """
+        Compute the error created by the DD_Privacy and the error if using a naive geometric noise
+        return a list with these errors: [error, error_naive]
+    """
+    error = 0
+    error_naive = 0
+    users = []
+    users_naive = []
+    for i in range(1, nb_users+1):
+        user = User(secrets.choice(data_group), sk[i])
+        users.append(user)
+        users_naive.append(user)
+
+    #list with the clear values
+    data_list = []
+
+    #Users add noise to their data
+    for i in range(0, len(users)):
+        data_list.append(users[i].x)
+        users[i].DD_Privacy()
+        error += (users[i].x - data_list[i])
+        users_naive[i].naive_scheme()
+        error_naive += (users_naive[i].x - data_list[i])
+        
+    return [error, error_naive]
 
 #############################################
-# TEST
+# TESTS AND SIMULATIONS
 #############################################
 
 #group definition
 G = IntegerGroup()
-G.paramgen(64)
+G.paramgen(14)
 
 #prime order
 p = int(G.q)
 print(f"\nGroup order: {p}")
-
+modulus = 2*p+1
+print(f"Modulus: {modulus}")
 
 #generator
 g = G.randomGen()
 print(f"Group generator: {g}")
-
-#number of users
-nb_users = 30
-print(f"Number of users: {nb_users}")
 
 #Hash value for time t
 t = 1
 ht = H(t)
 
 #text space
-#delta : limit value of user's data (x[i] in {0,2,...,delta})
-delta = 50
+#delta : limit value of user's data (user's value x[i] in {0,1,...,delta})
+delta = 1
 data_group = [i for i in range(0,delta+1)]
 
 # (eps, d)-DD-privacy parameters with delta >= eps/3 and 0 < d < 1
-eps = 20
-d = 0.01
+eps = 0.5
+d = 0.1
 
 #proportion of uncompromised participants
-gamma = 0.9
+gamma = 1
+
+#############################################
+# Test for a given time t and n users
+#############################################
+
+#Uncomment this section for one run at time t with the defined paramaters
+
+#number of users
+nb_users = 10000
+print(f"Number of users: {nb_users}")
 
 #secret keys
 sk = keygen(nb_users,p)
@@ -184,7 +240,8 @@ sum = 0
 for i in sk:
     sum = (sum+i)%p
 
-print(f"secret keys : {sk}")
+
+# print(f"secret keys : {sk}")
 print(f"sum of the secret keys : {sum}\n")
 
 
@@ -196,17 +253,64 @@ for i in range(1, nb_users+1):
 
 data_list = []
 expected_result = 0
+expected_result_with_noise = 0
 ciphertexts = []
+ciphertexts_noisy = []
+
 #Users add noise to their data
 for user in users:
-    # user.DD_Privacy()
-    data_list.append(user.x)
+    # data_list.append(user.x)
     expected_result = (expected_result + user.x)%p
     ciphertexts.append(user.encrypt())
+    user.DD_Privacy()
+    expected_result_with_noise = (expected_result_with_noise + user.x)%p
+    ciphertexts_noisy.append(user.encrypt())
 
 #Aggregator
 agg = Aggregator(ciphertexts, sk[0])
-print(f"list of all users'data (before encryption) : {data_list}")
-print(f"list of all ciphertexts (given to the aggregator) : {ciphertexts}\n")
-print(f"decrypted sum by the aggregator : {agg.decrypt()}")
+agg_noisy = Aggregator(ciphertexts_noisy, sk[0])
+decrypt = agg.decrypt()
+decrypt_noisy = agg_noisy.decrypt()
+error = abs(int(decrypt) - int(decrypt_noisy))
+# print(f"list of all users'data (before encryption) : {data_list}")
+# print(f"list of all ciphertexts (given to the aggregator) : {ciphertexts}\n")
 print(f"expected sum : {expected_result}")
+print(f"expected sum (with noise): {expected_result_with_noise}")
+print(f"decrypted sum by the aggregator : {decrypt}")
+print(f"decrypted sum by the aggregator with noise : {decrypt_noisy}")
+print(f"Error range : {error}")
+#############################################
+# DD privacy
+#############################################
+
+#Uncomment this section for simulation result, comparing the distributed differential privacy scheme against a naive scheme where geometric noise is added to all data participants independantly
+
+# x = [10**i for i in range(1,4)]
+# print(x)
+# y = [] #average error for x participants for our DD privacy scheme
+# y2= [] #average error for x participants for the naive scheme
+# result = []
+# nb_samples = 100 # mean calculated over nb_samples runs
+# for k in x:
+#     nb_users = k
+#     for j in range(0, nb_samples):
+#         sk = keygen(nb_users,p)
+#         mean = 0
+#         mean2 = 0
+#         result = error(nb_users, sk, data_group)
+#         mean += result[0]/nb_samples
+#         mean2 += result[1]/nb_samples
+#     y2.append(mean2)
+#     y.append(mean)
+
+# print(f"x = {x}")
+# print(f"average error for our DD privacy scheme = {y}")
+# print(f"average error for naive schme = {y2}")
+
+# fig, ax = plt.subplots()
+# ax.plot(x, y, linestyle='-', marker='o', color='b', label="Our scheme")
+# ax.plot(x, y2, linestyle='--', marker='o', color='r', label="Naive scheme")
+# ax.set_xlabel("Number of users")
+# ax.set_ylabel("Error")
+# ax.legend()
+# plt.show()
